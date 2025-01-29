50e353080ee722fb84dbcd05c88f43be59724e02
unknown <rafael.eng.mecanic@gmail.com>
2025-01-28 21:17:11 -0300

    add tailwind e primeng

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    CardModule,
+    CheckboxModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log('Login attempt', { username: this.username });
+    // Aqui você implementará a lógica de login
+  }
+}
