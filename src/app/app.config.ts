ed3d484b982ce3c85a19db7e436b96727f4aaeac
Mikael Medeiros <mikaeltruth@gmail.com>
2025-01-28 22:17:28 -0300

    login and theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,22 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
 
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{blue.50}',
+          100: '{blue.100}',
+          200: '{blue.200}',
+          300: '{blue.300}',
+          400: '{blue.400}',
+          500: '{blue.500}',
+          600: '{blue.600}',
+          700: '{blue.700}',
+      }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true })
@@ -15,8 +30,8 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
-        }
+            preset: MyPreset
+        },        
     })
   ]
 };
