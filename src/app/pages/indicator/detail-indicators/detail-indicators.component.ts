b9b6842550bdaecf35022b26ae04632daeb32f15
Mikael Medeiros <mikaeltruth@gmail.com>
2025-02-01 18:47:55 -0300

    edit on indicator table

--- src/app/pages/indicator/detail-indicators/detail-indicators.component.ts
+++ src/app/pages/indicator/detail-indicators/detail-indicators.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Indicator } from '../../../model/Indicator';
 import { TableModule } from 'primeng/table';
+import { SelectModule } from 'primeng/select';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-indicators',
-  imports: [TableModule],
+  imports: [CommonModule, FormsModule , TableModule, SelectModule],
   templateUrl: './detail-indicators.component.html',
   styleUrl: './detail-indicators.component.css'
 })
@@ -12,6 +15,12 @@ export class DetailIndicatorsComponent {
   @Input() indicators: Indicator[] = [];
   id: string | null = null;
 
+  evidences: any[] = [
+    { name: 'Sim', code: true },
+    { name: 'Não', code: false },
+    { name: 'Não se aplica', code: null }
+  ];
+  selectedEvidence: boolean | null = null;
 
 
 
